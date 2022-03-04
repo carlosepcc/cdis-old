725a7480eef44200ec6c75d1888914a8d9c3692a
Zero9738 <94548236+Zero9738@users.noreply.github.com>
2022-03-03 21:34:16 -0500

    Update CreateRoles.java

--- src/main/java/com/pid/proyecto/util/CreateRoles.java
+++ src/main/java/com/pid/proyecto/util/CreateRoles.java
@@ -18,16 +18,16 @@ public class CreateRoles implements CommandLineRunner {
 
     @Autowired
     PasswordEncoder passwordEncoder;
-    
+
     @Autowired
     RolSistemaService rolSistemaService;
-    
+
     @Autowired
     UsuarioService usuarioService;
 
     @Override
     public void run(String... args) throws Exception {
-        
+
         // crear comision disciplinaria
         // crear denuncia
         // crear caso disciplinario
@@ -35,31 +35,28 @@ public class CreateRoles implements CommandLineRunner {
         // crear declaracion
         // crear usuario
         RolSistema rolAdmin = new RolSistema(RolNombre.ROLE_ADMIN);
-        
+
         // crear comision disciplinaria
         // crear denuncia
         // crear caso disciplinario
         RolSistema rolDecano = new RolSistema(RolNombre.ROLE_DECANO);
-        
+
         // crear denuncia
         RolSistema rolProfesor = new RolSistema(RolNombre.ROLE_PROFESOR);
-        
+
         // crear denuncia
         RolSistema rolTrabajador = new RolSistema(RolNombre.ROLE_TRABAJADOR);
-        
+
         // crear declaracion
         RolSistema rolEstudiante = new RolSistema(RolNombre.ROLE_ESTUDIANTE);
-        
+
         RolSistema rolUser = new RolSistema(RolNombre.ROLE_USER);
 
         RolSistema rolIntegrante = new RolSistema(RolNombre.ROLE_INTEGRANTE);
         RolSistema rolPresidente = new RolSistema(RolNombre.ROLE_PRESIDENTE);
         RolSistema rolSecretario = new RolSistema(RolNombre.ROLE_SECRETARIO);
 
-        Set<RolSistema> roles = new HashSet<>();
-        roles.add(rolSistemaService.getByRol(RolNombre.ROLE_ADMIN).get());
-        Usuario usuario = new Usuario("nombreadmin", "apellidosadmin", "admin", passwordEncoder.encode("admin"), false);
-        usuario.setRolsistemaList(roles);
+        
 
         if (!rolSistemaService.existsByRol(RolNombre.ROLE_ADMIN)) {
             rolSistemaService.save(rolAdmin);
@@ -90,11 +87,16 @@ public class CreateRoles implements CommandLineRunner {
         if (!rolSistemaService.existsByRol(RolNombre.ROLE_ESTUDIANTE)) {
             rolSistemaService.save(rolEstudiante);
         }
+
+        Set<RolSistema> roles = new HashSet<>();
+
+        Usuario usuario = new Usuario("nombreadmin", "apellidosadmin", "admin", passwordEncoder.encode("admin"), false);
+        roles.add(rolSistemaService.getByRol(RolNombre.ROLE_ADMIN).get());
+        usuario.setRolsistemaList(roles);
         // CREAMOS UN ADMINISTRADOR POR DEFECTO SI LA BASE DE DATOS ESTA VACÍA
         if (!usuarioService.existsById(1)) {
             usuarioService.save(usuario);
         }
-        
     }
 
 }
