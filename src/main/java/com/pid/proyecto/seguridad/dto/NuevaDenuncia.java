991edbeb38a724114b06eb3b900a281c04281926
Zero9738 <94548236+Zero9738@users.noreply.github.com>
2022-03-03 22:06:15 -0500

    arreglos en denuncia

--- src/main/java/com/pid/proyecto/seguridad/dto/NuevaDenuncia.java
+++ src/main/java/com/pid/proyecto/seguridad/dto/NuevaDenuncia.java
@@ -1,6 +1,5 @@
 package com.pid.proyecto.seguridad.dto;
 
-import com.pid.proyecto.entity.Usuario;
 import java.util.Date;
 import java.util.List;
 import javax.validation.constraints.NotBlank;
@@ -8,8 +7,6 @@ import javax.validation.constraints.NotBlank;
 public class NuevaDenuncia {
 
     private String descripcion;
-    @NotBlank
-    private Date fecha;
     @NotBlank
     private String denunciante;
     @NotBlank
@@ -24,14 +21,6 @@ public class NuevaDenuncia {
 
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
-    }
-
-    public Date getFecha() {
-        return fecha;
-    }
-
-    public void setFecha(Date fecha) {
-        this.fecha = fecha;
     }
 
     public String getDenunciante() {
